6c3b4ee966715b892d83d89530754bc4aab37c42
SaladinX1 <hopefulhorizon01@gmail.com>
2021-12-23 15:04:15 +0100

    réctification de la route get pour /sauces/:id

--- routes/Sauce.js
+++ routes/Sauce.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/sauces', multer_config, sauceCtrl.sauceCreation);
 router.get('/sauces', sauceCtrl.saucesFind);
-router.get('sauces/:id', sauceCtrl.uniqueSauce);
+router.get('/sauces/:id', sauceCtrl.uniqueSauce);
 //router.put('/sauces/:id ', sauceCtrl.sauceUpdate);
 //router.delete('/sauces/:id', sauceCtrl.sauceDelete);
 //router.post('/sauces/:id/like', sauceCtrl.likeDislike);
