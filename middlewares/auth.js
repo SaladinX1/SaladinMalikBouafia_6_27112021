9cc369ef50f6b27ac3addf5b83bc5a594e29d917
SaladinX1 <hopefulhorizon01@gmail.com>
2021-12-30 14:05:10 +0100

    Correction du middleware d'authentification

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
 
     try {
-        const token = req.headers.authorisation.split(' ')[1];
+        const token = req.headers.authorization.split(' ')[1];
         const VerifyToken = jwt.verify(token, 'RANDOM_SECRET_TOKEN_KEY');
         const userId = VerifyToken.userId;
         if (req.body.userId && req.body.userId != userId) {
